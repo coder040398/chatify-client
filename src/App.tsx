6e0571f05acb00c146187cec6d24b3419025efad
Rino <coder040398@gmail.com>
2023-02-12 17:59:51 +0700

    update App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment } from 'react';
+import { Routes, Route, Outlet } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <Fragment>
+      <Routes>
+        <Route path='/' element={<div>Hello, Home!</div>} />
+        <Route
+          path='conversations'
+          element={
+            <div>
+              <div>Conversation</div>
+              <Outlet />
+            </div>
+          }
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <Route path=':id' element={<div>Conversation ID Page</div>} />
+        </Route>
+      </Routes>
+    </Fragment>
   );
 }
 
